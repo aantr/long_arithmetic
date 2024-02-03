928a6391cf707845a5b3c4469397280c3b545474
aantr <xgamestudio8@gmail.com>
2024-02-03 19:38:25 +0300

    fixed out

--- src/calculate_pi.cpp
+++ src/calculate_pi.cpp
@@ -92,7 +92,6 @@ int main() {
     LongDouble result = Chudnovsky(digits);
     stringstream ss;
     ss << setprecision(digits + 1) << result;
-    ss << setprecision(6) << result;
     string result_string = ss.str();
     cout << "PI:\n" << result_string << "\n";
     cout << setprecision(6);
