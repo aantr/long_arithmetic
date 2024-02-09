fc472241d6f49ff3719bd1380735ec76d3ea6f07
aantr <xgamestudio8@gmail.com>
2024-02-09 23:51:58 +0300

    arithmetic 32

--- include/defines.hpp
+++ include/defines.hpp
@@ -1,18 +1,2 @@
-#undef assert
-#define assert(x) 
-#pragma once
-
-#include <iostream>
-using namespace std;
-
-void _print();
-
-template <typename T, typename... V>
-void _print(T t, V... v);
-
-#define debug(...) {\
-	cerr<<"["<<#__VA_ARGS__<<"] = [";\
-	_print(__VA_ARGS__);\
-}
-
-
+// #undef assert
+// #define assert(x)
