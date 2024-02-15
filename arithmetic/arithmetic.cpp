72d804aeb8f706ecc7122cef5484f4c9c190a166
aantr <xgamestudio8@gmail.com>
2024-02-15 21:24:18 +0300

    fixed

--- arithmetic/arithmetic.cpp
+++ arithmetic/arithmetic.cpp
@@ -58,17 +58,16 @@ namespace arithmetic {
         throw NegativePowerError();
     }
 
-    LongDouble::LongDouble(LongDouble &&other) noexcept { // move;
-        if (this == &other) {
-            return;
-        }
-        cout << "move" << endl;
-        // swap(sign, other.sign);
-        // swap(digits, digits);
-        // swap(digits_size, other.digits_size);
-        // swap(precision, other.precision);
-        // swap(exponent, other.exponent);
-    }
+    // LongDouble::LongDouble(LongDouble &&other) noexcept { // move;
+    //     if (this == &other) {
+    //         return;
+    //     }
+    //     swap(sign, other.sign);
+    //     swap(digits, digits);
+    //     swap(digits_size, other.digits_size);
+    //     swap(precision, other.precision);
+    //     swap(exponent, other.exponent);
+    // }
 
     LongDouble::~LongDouble() {
         free(digits);
--- arithmetic/arithmetic_operators.cpp
+++ arithmetic/arithmetic_operators.cpp
@@ -79,17 +79,17 @@ namespace arithmetic {
         return *this;
     }
 
-    LongDouble& LongDouble::operator=(LongDouble &&other) noexcept { // move
-        if (this == &other) {
-            return *this;
-        }
-        sign = exchange(other.sign, 1);
-        digits = exchange(other.digits, nullptr);
-        digits_size = exchange(other.digits_size, 0);
-        precision = exchange(other.precision, default_precision);
-        exponent = exchange(other.exponent, 0);
-        return *this;
-    }
+    // LongDouble& LongDouble::operator=(LongDouble &&other) noexcept { // move
+    //     if (this == &other) {
+    //         return *this;
+    //     }
+    //     sign = exchange(other.sign, 1);
+    //     digits = exchange(other.digits, nullptr);
+    //     digits_size = exchange(other.digits_size, 0);
+    //     precision = exchange(other.precision, default_precision);
+    //     exponent = exchange(other.exponent, 0);
+    //     return *this;
+    // }
 
     LongDouble operator""_ld (const char* x, unsigned long size) {
         LongDouble res(x, max((unsigned long) LongDouble::default_precision, (size - 1) / LongDouble::base_exp + 1));
